031b30b94b9fb04b2328afc99dde2926a8b204e7
trocolit2 <trocolit2@gmail.com>
2016-01-21 10:18:27 -0300

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fps_per_velocity/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    fps_per_velocity::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
